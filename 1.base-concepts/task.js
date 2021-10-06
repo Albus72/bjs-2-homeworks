448db061f8abc7e7509b4f12c1b6ae4328472093
Albus72 <albus72@tut.by>
2021-10-06 16:52:28 +0300

    complet task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 function solveEquation(a, b, c) {
   let arr;
-    let Discriminant = b * b - 4 * a * c;
-    
-
+    arr = [];
+    if (a === 0) {
+      return arr = ['Т.к. а=0, это не квадратное уравнение'];
     }
-
+    let Discriminant = b * b - 4 * a * c;
+    if (Discriminant < 0) {
+      return arr = [];
+    }
+    if (Discriminant === 0) {
+      let x = (-b + Math.sqrt(Discriminant)) / (2 * a);
+      arr.push(x);
+    } else if (Discriminant > 0) {
+      let x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+      arr.push(x1);
+      let x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+      arr.push(x2);
+    }
   // код для задачи №1 писать здесь
   return arr; // array
 }
