c55c57da2f1dedd56eb5917488657261d12381c0
Albus72 <albus72@tut.by>
2021-10-08 12:43:45 +0300

    function worker complet

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,54 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  sum = avg = 0;
+  min = max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const item = worker(arrOfArr[i]);
+    if (item > max) {
+      max = item;
+    }
+  }  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let diff = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    diff = max - min;
+  }
 }
