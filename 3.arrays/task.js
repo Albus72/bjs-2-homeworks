b388e71102495f850300d0f77a4b07c1d22495f0
Albus72 <albus72@tut.by>
2021-10-10 11:55:48 +0300

    task complet

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
-  result = arr1.length == arr2.length && arr1.every((v,i) => v === arr2[i])
-  // if (arr1.length === arr2.length) {
+  result = arr1.length == arr2.length && arr1.every((value,item) => value === arr2[item]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter((value) => value > 0).filter((value) => value % 3 == 0).map((value) => value * 10);
   return resultArr; // array
 }
