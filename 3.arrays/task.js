f5cf4234b773de9246262349c95047fec35e9934
Albus72 <albus72@tut.by>
2021-10-10 10:05:33 +0300

    function compareArrays complet

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
-  if (arr1.length === arr2.length) {
-    
-  }
-  // Ваш код
-
+  result = arr1.length == arr2.length && arr1.every((v,i) => v === arr2[i])
+  // if (arr1.length === arr2.length) {
   return result; // boolean
 }
 
