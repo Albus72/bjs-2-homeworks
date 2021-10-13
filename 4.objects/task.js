65ad8119ff9307e2ce49ccd30362cbc80202a452
Albus72 <albus72@tut.by>
2021-10-14 02:19:28 +0300

    3 from 5 metods complet

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,12 +1,38 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
+  this.name = name,
+  this.gender = gender,
+  this.age = age
 
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if ( this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+  const summa = this.marks.reduce((sum, current) => sum + current, 0);
+  // const items = this.marks.lenght;
+  return summa;
+}
+
+Student.prototype.addMarks = function(...mark) {
+  this.marks = [...mark];
+}
+
+Student.prototype.getAverage = function() { 
+  this.average = summa / 3;
+}
+
+Student.prototype.exclude = function(reason) {
+  if (Student.average < 3)  {
+    this.excluded = reason;
+  }
+}
+
+
+
