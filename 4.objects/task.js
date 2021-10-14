0840b8993b96c38b1c5ecec447dad7f2f59b30fa
Albus72 <albus72@tut.by>
2021-10-14 11:21:22 +0300

    4 from 5 tasks complet

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,23 +15,23 @@ Student.prototype.addMark = function(mark) {
   } else {
     this.marks.push(mark);
   }
-  const summa = this.marks.reduce((sum, current) => sum + current, 0);
-  // const items = this.marks.lenght;
-  return summa;
 }
 
 Student.prototype.addMarks = function(...mark) {
   this.marks = [...mark];
 }
 
-Student.prototype.getAverage = function() { 
-  this.average = summa / 3;
-}
+Student.prototype.getAverage = function () {
+  const summa = this.marks.reduce((sum, current) => sum + current, 0);
+  this.arrAverage = summa / this.marks.length;
+};
 
 Student.prototype.exclude = function(reason) {
-  if (Student.average < 3)  {
+  // if (Student.arrAverage < 3)  {
     this.excluded = reason;
-  }
+    delete this.subject;
+    delete this.marks;
+  // }
 }
 
 
