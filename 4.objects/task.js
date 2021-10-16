50bbbd3813761cdd5d2af05af140ade3cba82a21
Albus72 <albus72@tut.by>
2021-10-16 12:25:29 +0300

    fixed task.js file

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -18,8 +18,16 @@ Student.prototype.addMark = function(mark) {
 }
 
 Student.prototype.addMarks = function(...mark) {
-  this.marks = [...mark];
+  if ( this.marks === undefined) {
+    this.marks = [...mark];
+  } else {
+    this.marks.push([...mark]);
+  }
 }
+
+// Student.prototype.addMarks = function(...mark) {
+//   this.marks = [...mark];
+// }
 
 Student.prototype.getAverage = function() {
   this.arrAverage = this.marks.reduce((sum, current) => sum + current, 0) / this.marks.length;
