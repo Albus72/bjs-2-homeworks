bc8a781f4ba8e0c9d7fd55dcaaff5058d135bfc3
Albus72 <albus72@tut.by>
2021-10-18 17:53:55 +0300

    Решена задача 1.2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,26 +1,42 @@
 function parseCount(parcedNumber) {
-    const tempVar = Number.parseInt(parcedNumber);    
-    if (Number.isNaN(tempVar) === true) {
-        console.log(tempVar);
-        throw new Error('Невалидное значение');
-    } else {
-        return tempVar;
-    }
+	const tempVar = Number.parseInt(parcedNumber, 10);
+	if (Number.isNaN(tempVar) === true) {
+		throw new Error('Невалидное значение');
+	} else {
+		return tempVar;
+	}
 }
 
-function validateCount(validateCount) {
-    const tempVar = parseCount(validateCount); 
-    try {
-        if (Number.isNaN(tempVar) === false) {
-            return tempVar;
-        }
-    } catch(err) {
-        if (Number.isNaN(tempVar) === true) {
-            console.log(err);
-            throw new Error('Невалидное значение');
+function validateCount(parcedNumber2) {
+	try {
+		const tempVar2 = parseCount(parcedNumber2);
+		return tempVar2;
+	} catch (err) {
+		console.log('' + err.message);
+        return err;
+	}
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
+    }
+
+    catchAnException() {
+        console.log(a, b, c);
+        console.log(this.sideA);
+        if ((this.sideA + this.sideB < this.sideC) || (this.sideA + this.sideC < this.sideB) || (this.sideB + this.sideC < this.sideA)) {
+            throw new Error('Треугольник с такими сторонами не существует'); 
         }
     }
 
-    
+    getPerimeter() {
+        const perimeter = this.sideA + this.sideB + this.sideC;
+        console.log(perimeter);
+        return perimeter;
+    }
+
 
 }
