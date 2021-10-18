a44a9a9593c694e84d577af718ecfcac2da0f3aa
Albus72 <albus72@tut.by>
2021-10-18 12:20:46 +0300

    Решена задача 1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,3 +1,26 @@
-function parseCount()
+function parseCount(parcedNumber) {
+    const tempVar = Number.parseInt(parcedNumber);    
+    if (Number.isNaN(tempVar) === true) {
+        console.log(tempVar);
+        throw new Error('Невалидное значение');
+    } else {
+        return tempVar;
+    }
+}
 
-function validateCount()
+function validateCount(validateCount) {
+    const tempVar = parseCount(validateCount); 
+    try {
+        if (Number.isNaN(tempVar) === false) {
+            return tempVar;
+        }
+    } catch(err) {
+        if (Number.isNaN(tempVar) === true) {
+            console.log(err);
+            throw new Error('Невалидное значение');
+        }
+    }
+
+    
+
+}
