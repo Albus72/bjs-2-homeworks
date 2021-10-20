94baabd3d8d29c0a2b5a1ad1c50e2e968094f37b
Albus72 <albus72@tut.by>
2021-10-20 22:12:09 +0300

    Заработали методы Perimeter & Area

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -25,29 +25,20 @@ class Triangle {
         console.log(this.sideA, this.sideB, this.sideC);
         if ((this.sideA + this.sideB < this.sideC) || (this.sideA + this.sideC < this.sideB) || (this.sideB + this.sideC < this.sideA)) {
             throw new Error('Треугольник с такими сторонами не существует'); 
+            
         } 
     }
 
     getPerimeter() {
         const perimeter = this.sideA + this.sideB + this.sideC;
-        console.log(perimeter);
-        
-        if ()
-        //     console.log('Ошибка! Треугольник не существует' + err.message);
         return perimeter;
     }
 
     getArea() {
-        const p = this.perimeter / 2;
-        // const square = Number((Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))).toFixed(3));
-        const square = Math.round((Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))) *1000) / 1000;
-
+        const p = this.getPerimeter() / 2;
+        const square = Number((Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))).toFixed(3));
+        // const square = Math.round((Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC))) *1000) / 1000;
         return square;
-        // try {
-        //     return +square.toFixed(3);
-        // } catch (err) {
-        //     console.log('Ошибка! Треугольник не существует');
-        // }     
     }
 }
 
@@ -56,7 +47,6 @@ function getTriangle(a,b,c) {
     try {
 		return triangle;
 	} catch (err) {
-		console.log('' + err.message);
-        return triangle.getPerimeter, triangle.getArea;
+		return {getPerimeter: "Ошибка! Треугольник не существует", getArea: "Ошибка! Треугольник не существует"};
 	}
 }
