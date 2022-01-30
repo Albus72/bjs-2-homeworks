2f2febdd9a00165d0cfa8dd4a33eb4bad82d56c3
Albus72 <albus72@tut.by>
2022-01-30 11:43:49 +0300

    Задача 2 выполнена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,26 +6,38 @@ function solveEquation(a, b, c) {
       return arr = ['Т.к. а=0, это не квадратное уравнение'];
     }
     let Discriminant = b * b - 4 * a * c;
-    // if (Discriminant < 0) {
-    //   return arr = [];
-    // }
     if (Discriminant === 0) {
       let x = (-b + Math.sqrt(Discriminant)) / (2 * a);
       arr.push(x);
     } else if (Discriminant > 0) {
       let x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
-      // arr.push(x1);
       let x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
       arr.push(x1, x2);
     }
-  // код для задачи №1 писать здесь
-  return arr; // array
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  
+  if (isNaN(percent)) {
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  } else
+  if (isNaN(contribution)) {
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  } else
+  if (isNaN(amount)) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  };
+  
+  percent = Number(percent / 100);
+  contribution = Number(contribution);
+  amount = Number(amount);
 
-  // код для задачи №2 писать здесь
+  const creditPeriod = Math.trunc(((date - Date.now()) / 8.64e7) / 30); //Принимаем, что в среднем месяц = 30 дням
+  const loanAmount = amount - contribution;
+  const monthlyPayment = loanAmount * ((percent / 12) + (percent / 12) / (((1 + (percent / 12)) ** creditPeriod) - 1));
 
+  console.log(totalAmount = Number((monthlyPayment * creditPeriod).toFixed(2)));
   return totalAmount;
 }
