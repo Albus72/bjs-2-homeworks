dccdc67c6dd9f884206eb7382e0777b2ec592d2d
Albus72 <albus72@tut.by>
2022-02-06 17:50:50 +0300

    Доработка task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,12 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  result = arr1.length == arr2.length && arr1.every((value,item) => value === arr2[item]);
-  return result; // boolean
+  return result = arr1.length == arr2.length && arr1.every((value,item) => value === arr2[item]);
 }
 
 function advancedFilter(arr) {
-  // let resultArr;
-  // resultArr = arr.filter((value) => value > 0).filter((value) => value % 3 == 0).map((value) => value * 10);
-  // return resultArr; // array
   return arr.filter((value) => value > 0).filter((value) => value % 3 == 0).map((value) => value * 10);
 }
