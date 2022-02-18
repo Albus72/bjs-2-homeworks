3483126250a41466834719263d3d8ba3bf78d2bd
Albus72 <albus72@tut.by>
2022-02-18 22:33:31 +0300

    Начинаем декорировать

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,33 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+  function wrapper(...args) {
+      const hash = args.join(','); // получаем правильный хэш
+      let idx = cache.findIndex((item) => item === cache[hash]); // ищем элемент, хэш которого равен нашему хэшу
+      if (idx !== -1 ) { // если элемент не найден
+          console.log("Из кэша: " + cache[idx]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+          return "Из кэша: " + cache[idx];
+      } else {
+          let result = func.call(this, ...args); // в кэше результата нет - придётся считать
+          cache[hash] = result;
+          cache.push(result); // добавляем элемент с правильной структурой
+          if (cache.length > 5) { 
+            cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+          }
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result;  
+      }
+  }
+  return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+
+
+// function debounceDecorator2(func) {
+//   // Ваш код
+// }
