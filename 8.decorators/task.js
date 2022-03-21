ab3219fd2dffb98cd6221317ba3ed3751bab0f12
Albus72 <albus72@tut.by>
2022-03-22 00:27:44 +0300

    Завершена корректировка

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -18,70 +18,39 @@ function cachingDecoratorNew(func) {
   };
 }
 
-function debounceDecoratorNew(func, ms) {
+function debounceDecoratorNew(func, ms){
   let timer;
-  return function (...args) {
+  let firstTime = true;
+  return function(...args) {
     clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, ...args);
-    }, ms);
+    if (firstTime) {
+      func.apply(this, args);
+      firstTime = false;
+    } else {
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, ms);
+    }
   };
-}
+};
 
-// function debounceDecorator2(func, ms) {
-//   let timer;
-//   function wrapper(...args) {
-//     clearTimeout(timer);
-//     timer = setTimeout(() => {
-//       func.apply(this, ...args);
-//     }, ms);
-//     wrapper.count++;
-//     console.log(wrapper.count);
-//   }
-//   wrapper.count = 0;
-//   return wrapper;
-// }
+function debounceDecorator2(func, ms){
+  let timer;
+  let firstTime = true;
+  let counter = 0;
 
-// function debounceDecorator2(func, ms) {
-//   let timer;
-//   let toggle = false;
-//   count = 0;
-//   return function (...args) {
-//     if (!toggle) {
-//       func(...args);
-//       toggle = true;
-//       timer = setTimeout(() => {
-//         toggle = false;
-//       }, ms);
-//     } else {
-//       clearTimeout(timer);
-//       timer = setTimeout(() => {
-//         toggle = false;
-//         console.timeEnd('time');
-//       }, ms);
-//     }
-//     count++;
-//     console.log(count);
-//   };
-// }
-// console.time('time');
+  function wrapper(...args) {
+    wrapper.count = ++counter;
+    clearTimeout(timer);
+    if (firstTime) {
+      func.apply(this, args);
+      firstTime = false;
+    } else {
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, ms);
+    }
+  };
 
-const sendSignal = () => console.log(`Сигнал отправлен`);
-const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-setTimeout(upgradedSendSignal); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
-setTimeout(upgradedSendSignal, 900); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 1200); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 2300); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 4500); // проигнорировано аналогично
-
-// const sendSignal = () => console.log(`Сигнал отправлен ${wrapper.count}`);
-// const upgradedSendSignal = debounceDecorator2(sendSignal, 2000);
-// setTimeout(upgradedSendSignal); // Сигнал отправлен
-// setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
-// setTimeout(upgradedSendSignal, 900); // проигнорировано аналогично
-// setTimeout(upgradedSendSignal, 1200); // проигнорировано аналогично
-// setTimeout(upgradedSendSignal, 2300); // проигнорировано аналогично
-// setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен
-// setTimeout(upgradedSendSignal, 4500); // проигнорировано аналогично
+  return wrapper;
+};
