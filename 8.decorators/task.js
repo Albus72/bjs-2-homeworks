f8320c83605cc3bd99f2b110b0e4c078c242e85b
Albus72 <albus72@tut.by>
2022-03-22 13:55:48 +0300

    Доработано после проверки

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -5,42 +5,22 @@ function cachingDecoratorNew(func) {
     if (hash in cache) {
       console.log('Из кэша: ' + cache[hash]);
       return 'Из кэша: ' + cache[hash];
-    } else {
-      let result = func(...args);
-      cache[hash] = result;
-      if (Object.keys(cache).length > 5) {
-        const key = Object.keys(cache);
-        delete cache[key[0]];
-      }
-      console.log('Вычисляем: ' + result);
-      return 'Вычисляем: ' + result;
     }
-  };
-}
-
-function debounceDecoratorNew(func, ms){
-  let timer;
-  let firstTime = true;
-  return function(...args) {
-    clearTimeout(timer);
-    if (firstTime) {
-      func.apply(this, args);
-      firstTime = false;
-    } else {
-      timer = setTimeout(() => {
-        func.apply(this, args);
-      }, ms);
+    let result = func(...args);
+    cache[hash] = result;
+    if (Object.keys(cache).length > 5) {
+      const key = Object.keys(cache);
+      delete cache[key[0]];
     }
-  };
+    console.log('Вычисляем: ' + result);
+    return 'Вычисляем: ' + result;
+  }
 };
 
-function debounceDecorator2(func, ms){
+function debounceDecoratorNew(func, ms) {
   let timer;
   let firstTime = true;
-  let counter = 0;
-
-  function wrapper(...args) {
-    wrapper.count = ++counter;
+  return function (...args) {
     clearTimeout(timer);
     if (firstTime) {
       func.apply(this, args);
@@ -50,7 +30,24 @@ function debounceDecorator2(func, ms){
         func.apply(this, args);
       }, ms);
     }
-  };
+  }
+};
 
+function debounceDecorator2(func, ms) {
+  let timer;
+  let firstTime = true;
+  function wrapper(...args) {
+    wrapper.counter = ++wrapper.counter;
+    clearTimeout(timer);
+    if (firstTime) {
+      func.apply(this, args);
+      firstTime = false;
+    } else {
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, ms);
+    }
+  }
+  wrapper.counter = 0;
   return wrapper;
 };
